69eeef396ce06931cea4dd0bb95f054b681ee884
sACHIN <sachinkrazzy4@gmail.com>
2019-02-24 13:22:05 +0000

    UART code for ATMEGA32A with Makefile

---
+++ UART/main.c
@@ -0,0 +1,13 @@
+// main.c
+#include <string.h>
+#include "uart.h"
+
+int main()
+{
+	uart_init(9600);
+	uart_puts("Hello DESD!!\r\n");
+	uart_puts("God Bless You!!\r\n");
+	while(1);
+	return 0;
+}
+
